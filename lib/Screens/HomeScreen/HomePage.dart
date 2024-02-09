f6c0935ec01294c60e7ca29f7dc5bd54bea826b7
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2024-02-09 22:10:30 +0530

    updated and got updates too

--- lib/Screens/HomeScreen/HomePage.dart
+++ lib/Screens/HomeScreen/HomePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import '../../Contants/app_style.dart';
 import '../../widgets/SizeConfig.dart';
